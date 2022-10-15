1284f6565bed9e343c976205dc441b837773b4dd
zaenb Al Ameri <89505056+Zaenb@users.noreply.github.com>
2022-10-15 02:40:54 -0500

    Update main.swift

--- main.swift
+++ main.swift
@@ -8,44 +8,24 @@ let unsortedIntegers = [5, 1, 4, 2, 8]
 
 
 // Add your code below:
-func selectionSort(unsortedIntegers: [Int]) -> [Int] {
-    var array = unsortedIntegers
-    var totalSwap = 0
-    var passCount = 0
-    var swapCountPerPass: Int = 0
-    print("Pass: \(passCount), Swaps: \(swapCountPerPass)/\(totalSwap),", terminator: " ")
-    print("Array: \(unsortedIntegers)")
-
-    for i in 0..<array.count - 1  {
-        swapCountPerPass = 0
-        var min = i
-        
-        for j in i + 1..<array.count {
-            if array[i] > array[min] {
-                min = j
-            }
-        }
-        
-        if array[i] > array[min]{
-            let temp = array[i]
-            array[i] = array[min]
-            array[min] = temp
-        }
-        swapCountPerPass += 1
-            totalSwap += 1
-            swapCountPerPass += 1
-        
-     
-       
-        passCount += 1
-
-        print("Pass: \(passCount), Swaps: \(swapCountPerPass)/\(totalSwap),", terminator: " ")
-        print("Array: \(array)")
-
-    }
-    return array
-}
-let SortedIntegers = selectionSort(unsortedIntegers: unsortedIntegers)
-//print("(SortedIntegers)")
-        
-        
+func insertionSort(unsortedIntegers: [Int]) -> [Int] {                                                                                                                          
+    var array = unsortedIntegers ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩  ⇩                                                          
+    var totalSwap = 0                                                                                                                                                           
+      var passCount = 0                                                                                                                                                           
+       var swapCountPerPass: Int = 0      LINES =========={M{E{R{L{I{N{1}N}I}L}R}E}M}=====================================                                                         
+      print("Pass: \(passCount), Swaps: \(swapCountPerPass)/\(totalSwap),", terminator: " ")                                                                                      
+        print("Array: \(unsortedIntegers)")                                                                                                                                         
+                                                                                                                                                                                
+                                                                                                                                                                                  
+                                                                                                                                                                                 
+      for i in 1..<array.count {                                                                                                                                                         swapCountPerPass = 0                                                                                                                                                    
+       let key = array[i]                                                                                                                                                      
+         var j = i - 1                                                                                                                                                           
+           while j>=0 && array[j] > key{                                                                                                                                           
+             array[j+1] = array[j]                                                                                                                                               
+              j = j - 1                                                                                                                                                           
+             totalSwap += 1                                                                                                                                                      
+             swapCountPerPass += 1                                                                                                                                               
+        }                                                                                                                                                                       
+         array[j+1] = key                                                                                                                                                        
+          passCount += 1                                     
